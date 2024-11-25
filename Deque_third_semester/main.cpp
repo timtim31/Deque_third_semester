6953c39f71dbb68dc29332a7d2c7d79eaa4bb8de
timtim31 <125752579+timtim31@users.noreply.github.com>
2024-11-25 23:31:48 +0300

    Update main.cpp

--- Deque_third_semester/main.cpp
+++ Deque_third_semester/main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include "..\Solver\List.h"
 
-int main()
+int main() 
 {
-	return 0;
+    List<int> list; 
+    int choice;
+
+    while (true) 
+    {
+        std::cout << "Выберите действие:\n";
+        std::cout << "1. Добавить элементы\n";
+        std::cout << "2. Удалить элемент\n";
+        std::cout << "3. Показать список\n";
+        std::cout << "4. Выйти\n";
+        std::cin >> choice;
+
+        switch (choice) 
+        {
+            case 1:
+                list.inputElements();
+                break;
+            case 2:
+                list.removeElementByUser();
+                break;
+            case 3:
+                list.print();
+                break;
+            case 4:
+                return 0;
+            default:
+                std::cout << "Неверный выбор\n";
+        }
+    }
+
+    return 0;
 }
