abb41ec8ce4d5134243daf809dcb1d433ba13c72
timtim31 <125752579+timtim31@users.noreply.github.com>
2024-12-16 20:32:18 +0300

    Update Solver.Test.cpp

--- Solver.Test/Solver.Test.cpp
+++ Solver.Test/Solver.Test.cpp
@@ -4,104 +4,96 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace SolverTest
+namespace Solvertest
 {
-    namespace ListTests
+    TEST_CLASS(ListTest)
     {
-        TEST_CLASS(SolverTest)
+    public:
+        TEST_METHOD(AddElements_IntData_Success)
         {
-        public:
-
-            TEST_METHOD(TestMethod1)
-            {
-            }
-        };
-
-        TEST_CLASS(ListTest)
+            List<int> list; 
+            list.add(1);
+            list.add(2);
+            list.add(3);
+            Assert::AreEqual(size_t(3), list.getSize());
+            Assert::AreEqual(1, list.get(0));
+            Assert::AreEqual(2, list.get(1));
+            Assert::AreEqual(3, list.get(2));
+        }
+    
+        TEST_METHOD(RemoveElements_IntData_Success)
         {
-        public:
-            TEST_METHOD(AddElements_IntData_Success)
+            List<int> list; 
+            list.add(1);
+            list.add(2);
+            list.add(3);
+            list.remove(1);
+            Assert::AreEqual(size_t(2), list.getSize());
+            Assert::AreEqual(1, list.get(0));
+            Assert::AreEqual(3, list.get(1));
+        }
+    
+        TEST_METHOD(Remove_OutOfRange_ThrowsException)
+        {
+            List<int> list;
+            Assert::ExpectException<std::out_of_range>([&]() { list.remove(0); });
+        }
+    
+        TEST_METHOD(Get_EmptyList_ThrowsException)
+        {
+            List<int> list;
+            Assert::ExpectException<std::out_of_range>([&]() { list.get(0); });
+        }
+    
+        TEST_METHOD(Resize_IntData_Success)
+        {
+            List<int> list; 
+            for (int i = 0; i < 100; ++i) 
             {
-                List<int> list; 
-                list.add(1);
-                list.add(2);
-                list.add(3);
-                Assert::AreEqual(size_t(3), list.getSize());
-                Assert::AreEqual(1, list.get(0));
-                Assert::AreEqual(2, list.get(1));
-                Assert::AreEqual(3, list.get(2));
+                list.add(i);
             }
-
-            TEST_METHOD(RemoveElements_IntData_Success)
-            {
-                List<int> list; 
-                list.add(1);
-                list.add(2);
-                list.add(3);
-                list.remove(1);
-                Assert::AreEqual(size_t(2), list.getSize());
-                Assert::AreEqual(1, list.get(0));
-                Assert::AreEqual(3, list.get(1));
-            }
-
-            TEST_METHOD(GetElement_IntData_Success)
-            {
-                List<int> list; 
-                list.add(1);
-                list.add(2);
-                list.add(3);
-                Assert::AreEqual(1, list.get(0));
-                Assert::AreEqual(2, list.get(1));
-                Assert::AreEqual(3, list.get(2));
-            }
-
-            TEST_METHOD(Resize_IntData_Success)
-            {
-                List<int> list; 
-                for (int i = 0; i < 100; ++i) 
-                {
-                    list.add(i);
-                }
-                Assert::AreEqual(size_t(100), list.getSize());
-                Assert::AreEqual(99, list.get(99));
-            }
-
-            TEST_METHOD(ToString_IntData_Success)
-            {
-                List<int> list; 
-                list.add(1);
-                list.add(2);
-                list.add(3);
-                Assert::AreEqual(std::string("1 2 3"), list.toString());
-            }
-
-            TEST_METHOD(InputElements_IntData_Success)
-            {
-                List<int> list; 
-                std::istringstream input("3\n4\n5\n6\n");
-                std::streambuf* cinbuf = std::cin.rdbuf(input.rdbuf()); 
-                list.inputElements();
-                std::cin.rdbuf(cinbuf); 
-                Assert::AreEqual(size_t(6), list.getSize());
-                Assert::AreEqual(4, list.get(3));
-                Assert::AreEqual(5, list.get(4));
-                Assert::AreEqual(6, list.get(5));
-            }
-
-            TEST_METHOD(RemoveElementByUser_IntData_Success)
-            {
-                List<int> list; 
-                list.add(1);
-                list.add(2);
-                list.add(3);
-                std::istringstream input("1\n");
-                std::streambuf* cinbuf = std::cin.rdbuf(input.rdbuf()); 
-                list.removeElementByUser();
-                std::cin.rdbuf(cinbuf); 
-                Assert::AreEqual(size_t(2), list.getSize());
-                Assert::AreEqual(1, list.get(0));
-                Assert::AreEqual(3, list.get(1));
-            }
-        };
-    }
+            Assert::AreEqual(size_t(100), list.getSize());
+            Assert::AreEqual(99, list.get(99));
+        }
+    
+        TEST_METHOD(ToString_EmptyList_Success)
+        {
+            List<int> list;
+            Assert::AreEqual(std::string(""), list.toString());
+        }
+    
+        TEST_METHOD(ToString_IntData_Success)
+        {
+            List<int> list; 
+            list.add(1);
+            list.add(2);
+            list.add(3);
+            Assert::AreEqual(std::string("1 2 3"), list.toString());
+        }
+        
+        TEST_METHOD(InputElements_IntData_Success)
+        {
+            List<int> list; 
+            std::istringstream input("3 4 5 6");
+            list.inputElements(input);
+            Assert::AreEqual(size_t(4), list.getSize());
+            Assert::AreEqual(3, list.get(0));
+            Assert::AreEqual(4, list.get(1));
+            Assert::AreEqual(5, list.get(2));
+            Assert::AreEqual(6, list.get(3));
+        }
+    
+        TEST_METHOD(RemoveElementByUser_IntData_Success)
+        {
+            List<int> list; 
+            list.add(1);
+            list.add(2);
+            list.add(3);
+            std::istringstream input("1");
+            list.removeElementByUser(input);
+            Assert::AreEqual(size_t(2), list.getSize());
+            Assert::AreEqual(1, list.get(0));
+            Assert::AreEqual(3, list.get(1));
+        }
+    };
 }
